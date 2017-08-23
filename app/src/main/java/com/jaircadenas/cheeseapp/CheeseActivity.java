3a91fe899053cef54e36690b98f7889b3075df0e
mussiojair <mussiojair@gmail.com>
2017-08-23 00:51:44 -0500

    Initial commit

---
+++ app/src/main/java/com/jaircadenas/cheeseapp/CheeseActivity.java
@@ -0,0 +1,203 @@
+package com.jaircadenas.cheeseapp;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraMetadata;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class CheeseActivity extends AppCompatActivity {
+
+
+    private final static String TAG = "CheeseAct";
+
+    private CameraManager mCameraManager;
+    private CameraDevice mCameraDevice;
+    private CameraDevice.StateCallback mCameraDeviceStateCallback;
+    private CameraCaptureSession.StateCallback mCameraCaptureSessionStateCallback;
+    private CameraCharacteristics mCameraCharacteristics;
+    private StreamConfigurationMap mStreamConfigMap;
+    private String mCameraId;
+    private TextureView mTextureView;
+    private TextureView.SurfaceTextureListener mTextureListener;
+    private Size mDimensionSize;
+    protected CaptureRequest.Builder captureRequestBuilder;
+    protected CameraCaptureSession cameraCaptureSessions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheese);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        mTextureView = (TextureView) findViewById(R.id.textureView);
+
+
+        mTextureListener = new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surfaceTexture, int i, int i1) {
+                setup();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surfaceTexture, int i, int i1) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surfaceTexture) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surfaceTexture) {
+
+            }
+        };
+
+        mTextureView.setSurfaceTextureListener(mTextureListener);
+
+
+        /* CameraCaptureSession.StateCallback */
+
+        mCameraCaptureSessionStateCallback = new CameraCaptureSession.StateCallback() {
+            @Override
+            public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                Log.e(TAG, "onConfigured");
+            }
+
+            @Override
+            public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                Log.e(TAG, "onConfigureFailed");
+            }
+        };
+
+        /* CameraDevice.StateCallback */
+
+        mCameraDeviceStateCallback = new CameraDevice.StateCallback() {
+            @Override
+            public void onOpened(@NonNull CameraDevice cameraDevice) {
+
+                mCameraDevice = cameraDevice;
+                Log.d(TAG, "camera onOpened");
+
+                try {
+                    SurfaceTexture texture = mTextureView.getSurfaceTexture();
+                    assert texture != null;
+                    texture.setDefaultBufferSize(mDimensionSize.getWidth(), mDimensionSize.getHeight());
+                    Surface surface = new Surface(texture);
+                    captureRequestBuilder = cameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                    captureRequestBuilder.addTarget(surface);
+                    cameraDevice.createCaptureSession(Arrays.asList(surface), new CameraCaptureSession.StateCallback(){
+                        @Override
+                        public void onConfigured(@NonNull CameraCaptureSession cameraCaptureSession) {
+                            //The camera is already closed
+                            if (null == mCameraDevice) {
+                                return;
+                            }
+                            // When the session is ready, we start displaying the preview.
+                            cameraCaptureSessions = cameraCaptureSession;
+                            updatePreview();
+                        }
+                        @Override
+                        public void onConfigureFailed(@NonNull CameraCaptureSession cameraCaptureSession) {
+                            Toast.makeText(CheeseActivity.this, "Configuration change", Toast.LENGTH_SHORT).show();
+                        }
+                    }, null);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onDisconnected(@NonNull CameraDevice cameraDevice) {
+
+            }
+
+            @Override
+            public void onError(@NonNull CameraDevice cameraDevice, int i) {
+
+            }
+        };
+
+
+    }
+
+    protected void updatePreview() {
+        if(null == mCameraDevice) {
+            Log.e(TAG, "updatePreview error, return");
+        }
+        captureRequestBuilder.set(CaptureRequest.CONTROL_MODE, CameraMetadata.CONTROL_MODE_AUTO);
+        try {
+            cameraCaptureSessions.setRepeatingRequest(captureRequestBuilder.build(), null, null);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // setup();
+    }
+
+    private void setup() {
+
+        try {
+            mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+            String[] camids = mCameraManager.getCameraIdList();
+            if (camids.length > 0) {
+                mCameraId = camids[1];
+            }
+
+            mCameraCharacteristics = mCameraManager.getCameraCharacteristics(mCameraId);
+            mStreamConfigMap = mCameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+            Size[] sizes = mStreamConfigMap.getOutputSizes(SurfaceTexture.class);
+            mDimensionSize = sizes[0];
+
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return;
+            }
+
+            mCameraManager.openCamera(mCameraId, mCameraDeviceStateCallback, null);
+
+        }catch( CameraAccessException cae ){
+
+            cae.printStackTrace();
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/jaircadenas/cheeseapp/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.jaircadenas.cheeseapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        ScheduledExecutorService schedule = Executors.newSingleThreadScheduledExecutor();
+        schedule.schedule(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent( SplashActivity.this, CheeseActivity.class );
+                startActivity(i);
+            }
+        }, 3, TimeUnit.SECONDS );
+    }
+
+}
